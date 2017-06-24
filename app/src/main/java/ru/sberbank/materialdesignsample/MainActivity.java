5b0f10ea3afe7a6bc0084ea939329f2cc0710333
Coyul <coenova@gmail.com>
2017-06-24 17:06:36 +0300

    material design views

--- app/src/main/java/ru/sberbank/materialdesignsample/MainActivity.java
+++ app/src/main/java/ru/sberbank/materialdesignsample/MainActivity.java
@@ -1,7 +1,11 @@
 package ru.sberbank.materialdesignsample;
 
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +14,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.sample_view);
+        setSupportActionBar((Toolbar)findViewById(R.id.toolbar));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        TextInputLayout textInputLayout = (TextInputLayout) findViewById(R.id.text_input_layout);
+        textInputLayout.setError("error");
+
+        android.support.v7.app.AlertDialog dialog = new AlertDialog.Builder(this)
+                .setTitle("title")
+                .setMessage("message")
+                .setPositiveButton("OK", null)
+                .setNegativeButton("NO", null)
+                .setNeutralButton("Whatever", null)
+                .create();
+        dialog.show();
+
+
+        Snackbar snackbar = Snackbar.make(findViewById(R.id.coordinator_layout), "some text", Snackbar.LENGTH_LONG);
+        snackbar.show();
     }
 }
